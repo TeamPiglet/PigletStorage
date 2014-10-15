4de73d9c61b18b9948d28546844d40c3f008a048
Krumov <krasin.krumov@gmail.com>
2014-10-15 18:37:33 +0300

    Image in details expands
    image expands and you can pinch zoom in and out

--- PigletStorage/src/com/example/pigletstorage/ListOfProducts.java
+++ PigletStorage/src/com/example/pigletstorage/ListOfProducts.java
@@ -6,6 +6,8 @@ import com.example.SQLite.SQLiteDataSource;
 import com.example.models.Product;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -14,54 +16,51 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
+@SuppressLint("NewApi") 
 public class ListOfProducts extends ActionBarActivity {
 
 	ListView list;
-    CustomAdapter adapter;
-    public  ListOfProducts CustomListView = null;
+	CustomAdapter adapter;
+	public ListOfProducts CustomListView = null;
 	private SQLiteDataSource datasource;
 	public List<Product> values;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list);
-	
-		  CustomListView = this;
-		  datasource = new SQLiteDataSource(CustomListView);
-		  datasource.open();
-		  
-		  values = datasource.getAllProducts();
-		  	   
-		  Resources res = getResources();
-		  list = ( ListView )findViewById( R.id.listView1 );  // List defined in XML ( See Below )
-		   
-		  /**************** Create Custom Adapter *********/
-		  adapter=new CustomAdapter( CustomListView, values,res );
-		  list.setAdapter( adapter );
+
+		ActionBar ab = getActionBar();
+		ab.setTitle("All Products");
+		
+		CustomListView = this;
+		datasource = new SQLiteDataSource(CustomListView);
+		datasource.open();
+
+		values = datasource.getAllProducts();
+
+		Resources res = getResources();
+		list = (ListView) findViewById(R.id.listOfAllProducts); // List defined in XML (
+														// See Below )
+
+		/**************** Create Custom Adapter *********/
+		adapter = new CustomAdapter(CustomListView, values, res);
+		list.setAdapter(adapter);
+
+	}
+
+	/****** Function to set data in ArrayList *************/
+
+	public void onItemClick(int mPosition) {
+		Product tempValues = (Product) values.get(mPosition);
+
+		Intent intent = new Intent(getBaseContext(),
+				ProductDetailsActivity.class);
+		intent.putExtra("product", tempValues);
+		startActivity(intent);
 	
 	}
 
-	  /****** Function to set data in ArrayList *************/
- 
-	
-    public void onItemClick(int mPosition){
-        Product tempValues = ( Product ) values.get(mPosition);
-
-        
-        Intent intent = new Intent(getBaseContext(), ProductDetailsActivity.class);
-        intent.putExtra("product", tempValues);
-        startActivity(intent);
-       // SHOW ALERT          
-
-//        Toast.makeText(CustomListView,
-//                	"Name: " + tempValues.getName()
-//                  + "\r\nPrice: " + tempValues.getPrice()
-//              	  +"\r\nType: " + tempValues.getType(),
-//					                Toast.LENGTH_LONG)
-//        .show();
-    }
-    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- PigletStorage/src/com/example/pigletstorage/ProductDetailsActivity.java
+++ PigletStorage/src/com/example/pigletstorage/ProductDetailsActivity.java
@@ -2,31 +2,52 @@ package com.example.pigletstorage;
 
 import com.example.models.Product;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ProductDetailsActivity extends ActionBarActivity {
-
+@SuppressLint("NewApi") public class ProductDetailsActivity extends FragmentActivity {
+	
+	private Animator mCurrentAnimator;    
+    private int mShortAnimationDuration;
+    
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_product_details);
 		
+		ActionBar ab = getActionBar();
+	    
+		
 		Product product = (Product) getIntent().getExtras().getParcelable("product");
 		
-		ImageView image = (ImageView)findViewById(R.id.imageDetail);
+		ab.setTitle(product.getName());
+		
+		final ImageButton image = (ImageButton)findViewById(R.id.imageDetail);
 		TextView name = (TextView)findViewById(R.id.nameValue);
 		TextView type = (TextView)findViewById(R.id.typeValue);
 		TextView price = (TextView)findViewById(R.id.priceValue);
 		TextView quantity = (TextView)findViewById(R.id.quantityValue);
 
-        Bitmap imageBitmap = BitmapFactory.decodeByteArray(product.getImage() , 0, product.getImage().length);
+        final Bitmap imageBitmap = BitmapFactory.decodeByteArray(product.getImage() , 0, product.getImage().length);
         
 		image.setImageBitmap(imageBitmap);		
 		name.setText(product.getName());
@@ -34,8 +55,128 @@ public class ProductDetailsActivity extends ActionBarActivity {
 		price.setText(product.getPrice());
 		quantity.setText(product.getQuantity());
 		
+		image.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomImageFromThumb(image, imageBitmap);
+            }
+        });
+		
+
+        mShortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
 	}
 
+	private void zoomImageFromThumb(final ImageButton thumbView, Bitmap imageResId) {
+        // If there's an animation in progress, cancel it immediately and proceed with this one.
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+
+        // Load the high-resolution "zoomed-in" image.
+        final TouchImageView expandedImageView = (TouchImageView) findViewById(R.id.expanded_image);
+        expandedImageView.setImageBitmap(imageResId);
+
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+ 
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.container).getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBounds.left,
+                        finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBounds.top,
+                        finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X, startScale, 1f))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimator = null;
+            }
+        });
+        set.start();
+        mCurrentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down to the original bounds
+        // and show the thumbnail instead of the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimator != null) {
+                    mCurrentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel, back to their
+                // original values.
+                AnimatorSet set = new AnimatorSet();
+                set
+                        .play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView, View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView, View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimator = set;
+            }
+        });
+    }
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
